224582013ff11a63e60457d3327853e80376a004
ymg2565626 <ymg2565626@stu.o-hara.ac.jp>
2026-05-07 14:25:03 +0900

    Update LoginExecuteAction.java

--- src/main/java/scoremanager/LoginExecuteAction.java
+++ src/main/java/scoremanager/LoginExecuteAction.java
@@ -1,32 +1,66 @@
 package scoremanager;
 
-import bean.Teacher;
-import dao.TeacherDAO;
+import java.util.ArrayList;
+import java.util.List;
+
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+
+import bean.Teacher;
+import dao.TeacherDao;
 import tool.Action;
+
 
 public class LoginExecuteAction extends Action {
 
-    public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+	@Override
+	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
 
-        String id = request.getParameter("id");
-        String password = request.getParameter("password");
+		//ローカル変数の宣言 1
+		String url = "";
+		String id = "";
+		String password = "";
+		TeacherDao teacherDao = new TeacherDao();
+		Teacher teacher = null;
 
-        TeacherDAO dao = new TeacherDAO();
-        Teacher teacher = dao.get(id);
+		//リクエストパラメータ―の取得 2
+		id = req.getParameter("id");// 教員ID
+		password = req.getParameter("password");//パスワード
 
-        if (teacher != null && teacher.getPassword().equals(password)) {
+		//DBからデータ取得 3
+		teacher = teacherDao.login(id, password);//教員データ取得
 
-            HttpSession session = request.getSession();
-            session.setAttribute("user", teacher); //ここ統一
+		//ビジネスロジック 4
+		//DBへデータ保存 5
+		//レスポンス値をセット 6
+		//フォワード 7
+		//条件で手順4~7の内容が分岐
+		if (teacher != null) {// 認証成功の場合
+			// セッション情報を取得
+			HttpSession session = req.getSession(true);
+			// 認証済みフラグを立てる
+			teacher.setAuthenticated(true);
+			// セッションにログイン情報を保存
+			session.setAttribute("user", teacher);
 
-            return "menu.jsp"; //returnで遷移
+			//リダイレクト
+			url = "main/Menu.action";
+			res.sendRedirect(url);
+		} else {
+			// 認証失敗の場合
+			// エラーメッセージをセット
+			List<String> errors = new ArrayList<>();
+			errors.add("IDまたはパスワードが確認できませんでした");
+			req.setAttribute("errors", errors);
+			// 入力された教員IDをセット
+			req.setAttribute("id", id);
 
-        } else {
-            request.setAttribute("error", "IDまたはパスワードが違います");
-            return "login.jsp"; //returnで戻る
-        }
-    }
+			//フォワード
+			url = "login.jsp";
+			req.getRequestDispatcher(url).forward(req, res);
+		}
+
+	}
+
 }
