7b8e647629a72037b355e102bba19c57c36f11fb
2565623 <ymg2565623@stu.o-hara.ac.jp>
2026-05-07 11:20:30 +0900

    Update Subject.java

--- src/main/java/bean/Subject.java
+++ src/main/java/bean/Subject.java
@@ -1,1 +1,35 @@
 
+package bean;
+
+import java.io.Serializable;
+
+public class Subject implements Serializable {
+
+    private String cd;
+    private String name;
+    private School school;
+
+    public String getId() {
+        return cd;
+    }
+
+    public void setId(String id) {
+        this.cd = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public School getSchool() {
+        return school;
+    }
+
+    public void setSchool(School school) {
+        this.school = school;
+    }
+}
