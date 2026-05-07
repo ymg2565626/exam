775812d97720e747785845534291f397654a849d
ymg2565626 <ymg2565626@stu.o-hara.ac.jp>
2026-05-07 14:03:10 +0900

    Update Action.java

--- src/main/java/tool/Action.java
+++ src/main/java/tool/Action.java
@@ -4,5 +4,5 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public abstract class Action {
-    public abstract String execute(HttpServletRequest request, HttpServletResponse response) throws Exception;
+    public abstract void execute(HttpServletRequest request, HttpServletResponse response) throws Exception;
 }
