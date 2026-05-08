941599c488199344211376bd14a418e2936629cf
ymg2565626 <ymg2565626@stu.o-hara.ac.jp>
2026-05-08 11:07:23 +0900

    Update SubjectDAO.java

--- src/main/java/dao/SubjectDAO.java
+++ src/main/java/dao/SubjectDAO.java
@@ -9,7 +9,7 @@ import java.util.List;
 import bean.School;
 import bean.Subject;
 
-public class SubjectDao extends Dao {
+public class SubjectDAO extends DAO {
 
     // 科目コード + 学校で1件取得
     public Subject get(String cd, School school) throws Exception {
