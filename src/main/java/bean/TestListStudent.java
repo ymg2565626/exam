615fcf398d668d0c64a3775577e43baa8e83f1bc
ymg2565626 <ymg2565626@stu.o-hara.ac.jp>
2026-05-11 12:23:45 +0900

    Update TestListStudent.java

--- src/main/java/bean/TestListStudent.java
+++ src/main/java/bean/TestListStudent.java
@@ -1,43 +1,82 @@
 package bean;
 
 public class TestListStudent {
-	
-	private String subjectName;
-	private String subjectCd;
-	private int num;
-	private int point;
-	
-	//getter,setter
-	public String getSubjectName() {
-		return subjectName;
-	}
-	
-	public void setSubjectName(String subjectName) {
-		this.subjectName=subjectName;
-	}
-	
-	public String getSubjectCd() {
-		return subjectCd;
-	}
-	
-	public void setSubjectCd(String subjectCd) {
-		this.subjectCd=subjectCd;
-	}
-	
-	public int getNum() {
-		return num;
-	}
-	
-	public void setNum(int num) {
-		this.num=num;
-	}
-	
-	public int getPoint() {
-		return point;
-	}
-	
-	public void setPoint(int point) {
-		this.point=point;
-	}
 
+    // 学生情報
+    private String studentNo;
+    private String studentName;
+    private String classNum;
+
+    // 科目情報
+    private String subjectName;
+    private String subjectCd;
+
+    // テスト情報
+    private int num;
+    private int point;
+
+    // getter,setter
+
+    // studentNo
+    public String getStudentNo() {
+        return studentNo;
+    }
+
+    public void setStudentNo(String studentNo) {
+        this.studentNo = studentNo;
+    }
+
+    // studentName
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    // classNum
+    public String getClassNum() {
+        return classNum;
+    }
+
+    public void setClassNum(String classNum) {
+        this.classNum = classNum;
+    }
+
+    // subjectName
+    public String getSubjectName() {
+        return subjectName;
+    }
+
+    public void setSubjectName(String subjectName) {
+        this.subjectName = subjectName;
+    }
+
+    // subjectCd
+    public String getSubjectCd() {
+        return subjectCd;
+    }
+
+    public void setSubjectCd(String subjectCd) {
+        this.subjectCd = subjectCd;
+    }
+
+    // num
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    // point
+    public int getPoint() {
+        return point;
+    }
+
+    public void setPoint(int point) {
+        this.point = point;
+    }
 }
