bcfe9ed30ac2ea07cdc1d1250defe2da30a8e40e
ymg2565607 <ymg2565607@stu.o-hara.ac.jp>
2026-05-11 10:24:09 +0900

    Update SubjectCreateAction.java

--- src/main/java/scoremanager/main/SubjectCreateAction.java
+++ src/main/java/scoremanager/main/SubjectCreateAction.java
@@ -4,14 +4,15 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import tool.Action;
 
-public class StudentCreateAction extends Action {
+public class SubjectCreateAction extends Action {
 
 	@Override
 	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
 
 
 		// JSPへフォワード 7
-		req.getRequestDispatcher("student_create.jsp").forward(req, res);
+		req.getRequestDispatcher("subject_create.jsp").forward(req, res);
 	}
 
 }
+
