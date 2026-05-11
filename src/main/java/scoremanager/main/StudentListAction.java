cae1655d29fe80936f588b1f05e9a1984554e602
ymg2565607 <ymg2565607@stu.o-hara.ac.jp>
2026-05-11 09:48:01 +0900

    Update StudentListAction.java

--- src/main/java/scoremanager/main/StudentListAction.java
+++ src/main/java/scoremanager/main/StudentListAction.java
@@ -1,9 +1,11 @@
 package scoremanager.main;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-import bean.ClassNum;
-import bean.School;
 import bean.Student;
 import bean.Teacher;
 import dao.ClassNumDAO;
@@ -15,73 +17,88 @@ import tool.Action;
 
 public class StudentListAction extends Action {
 
-    public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+	@Override
+	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
 
-        // セッション取得
-        HttpSession session = request.getSession();
+		HttpSession session = req.getSession();
+		Teacher teacher = (Teacher)session.getAttribute("user");
 
-        // ログイン情報取得
-        Teacher teacher = (Teacher) session.getAttribute("user");
+		// ローカル変数の指定 1
+		String entYearStr = ""; // 入力された入学年度
+		String classNum = ""; // 入力されたクラス番号
+		String isAttendStr = ""; // 入力された在学フラグ
+		int entYear = 0; // 入学年度
+		boolean isAttend = false; // 在学フラグ
+		List<Student> students = null; // 学生リスト
+		LocalDate todaysDate = LocalDate.now(); // LocalDateインスタンスを取得
+		int year = todaysDate.getYear(); // 現在の年を取得
+		StudentDAO studentDao = new StudentDAO(); // 学生Dao
+		ClassNumDAO classNumDao = new ClassNumDAO(); // クラス番号Daoを初期化
+		Map<String, String> errors = new HashMap<>(); // エラーメッセージ
 
-        if (teacher == null) {
-            System.out.println("【DEBUG】teacher is null");
-            request.getRequestDispatcher("login.jsp")
-            .forward(request, response);
-        }
+		// リクエストパラメーターの取得 2
+		entYearStr = req.getParameter("f1");
+		classNum = req.getParameter("f2");
+		isAttendStr = req.getParameter("f3");
 
-        // 学校取得
-        School school = teacher.getSchool();
+		// ビジネスロジック 4
+		if (entYearStr != null) {
+			// 数値に変換
+			entYear = Integer.parseInt(entYearStr);
+		}
+		if (isAttendStr != null) { // 在学フラグがnullじゃなかった場合
+			// 在学フラグをtrueに変換
+			isAttend = true;
+		}
+		// リストを初期化
+		List<Integer> entYearSet = new ArrayList<>();
+		// 10年前から1年後まで年をリストに追加
+		for (int i = year - 10; i < year + 1; i++) {
+			entYearSet.add(i);
+		}
 
-        if (school == null) {
-            System.out.println("【DEBUG】school is null");
-            request.getRequestDispatcher("error.jsp")
-            .forward(request, response); 
-        }
+		// DBからデータ取得 3
+		// ログインユーザーの学校コードをもとにクラス番号の一覧を取得
+		List<String> list = classNumDao.filter(teacher.getSchool());
 
-        System.out.println("【DEBUG】school_cd = " + school.getCd());
+		if (entYear != 0 && !classNum.equals("0")) {
+			// 入学年度とクラス番号を指定
+			students = studentDao.filter(teacher.getSchool(), entYear, classNum, isAttend);
+		} else if (entYear != 0 && classNum.equals("0")) {
+			// 入学年度のみ指定
+			students = studentDao.filter(teacher.getSchool(), entYear, isAttend);
+		} else if (entYear == 0 && classNum == null || entYear == 0 && classNum.equals("0")) {
+			// 指定なし
+			// 全学生情報を取得
+			students = studentDao.filter(teacher.getSchool(), isAttend);
+		} else {
+			errors.put("f1", "クラスを指定する場合は入学年度も指定してください");
+			// リクエストにエラーメッセージをセット
+			req.setAttribute("errors", errors);
+			// 全学生情報を取得
+			students = studentDao.filter(teacher.getSchool(), isAttend);
+		}
 
-        // パラメータ取得
-        String entYearStr = request.getParameter("entYear");
-        String classNum = request.getParameter("classNum");
-        String isAttendStr = request.getParameter("isAttend");
+		// レスポンス値をセット 6
+		// リクエストに入学年度をセット
+		req.setAttribute("f1", entYear);
+		// リクエストにクラス番号をセット
+		req.setAttribute("f2", classNum);
+		// 在学フラグが送信されていた場合
+		if (isAttendStr != null) {
+			// 在学フラグを立てる
+			isAttend = true;
+			// リクエストに在学フラグをセット
+			req.setAttribute("f3", isAttendStr);
+		}
+		// リクエストに学生リストをセット
+		req.setAttribute("students", students);
+		// リクエストにデータをセット
+		req.setAttribute("class_num_set", list);
+		req.setAttribute("ent_year_set", entYearSet);
 
-        System.out.println("【DEBUG】entYear = " + entYearStr);
-        System.out.println("【DEBUG】classNum = " + classNum);
-        System.out.println("【DEBUG】isAttendStr = " + isAttendStr);
+		// JSPへフォワード 7
+		req.getRequestDispatcher("student_list.jsp").forward(req, res);
+	}
 
-        // DAO
-        StudentDAO studentDAO = new StudentDAO();
-        ClassNumDAO classNumDAO = new ClassNumDAO();
-
-        List<Student> studentList;
-
-        // 在学フラグ
-        boolean isAttend = true;
-        if (isAttendStr != null && !isAttendStr.isEmpty()) {
-            isAttend = Boolean.parseBoolean(isAttendStr);
-        }
-
-        System.out.println("【DEBUG】isAttend = " + isAttend);
-
-        // ★まずは必ずデータが出るか確認（重要）
-        studentList = studentDAO.filter(school, isAttend);
-
-        System.out.println("【DEBUG】studentList size = " + studentList.size());
-
-        // クラス一覧
-        List<ClassNum> classNumList = classNumDAO.filter(school);
-
-        // requestにセット
-        request.setAttribute("studentList", studentList);
-        request.setAttribute("classNumList", classNumList);
-
-        // 検索条件も戻す
-        request.setAttribute("entYear", entYearStr);
-        request.setAttribute("classNum", classNum);
-        request.setAttribute("isAttend", isAttend);
-
-        // 画面へ
-        request.getRequestDispatcher("student_list.jsp")
-        .forward(request, response);
-    }
 }
