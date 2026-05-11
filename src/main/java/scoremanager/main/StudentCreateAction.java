ff7d55690b33226fac78e3638db2b005f76c94fa
ymg2565607 <ymg2565607@stu.o-hara.ac.jp>
2026-05-11 09:46:04 +0900

    Update StudentCreateAction.java

--- src/main/java/scoremanager/main/StudentCreateAction.java
+++ src/main/java/scoremanager/main/StudentCreateAction.java
@@ -1,8 +1,9 @@
 package scoremanager.main;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
-import bean.ClassNum;
 import bean.Teacher;
 import dao.ClassNumDAO;
 import jakarta.servlet.http.HttpServletRequest;
@@ -12,17 +13,39 @@ import tool.Action;
 
 public class StudentCreateAction extends Action {
 
-    public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+	@Override
+	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
 
-        HttpSession session = request.getSession();
-        Teacher teacher = (Teacher) session.getAttribute("user");
+		HttpSession session = req.getSession(); // セッション
+		Teacher teacher = (Teacher)session.getAttribute("user");
 
-        ClassNumDAO dao = new ClassNumDAO();
-        List<ClassNum> list = dao.filter(teacher.getSchool());
+		// ローカル変数の指定 1
+		ClassNumDAO classNumDao = new ClassNumDAO(); // クラス番号Daoを初期化
+		LocalDate todaysDate = LocalDate.now(); // LocalDateインスタンスを取得
+		int year = todaysDate.getYear(); // 現在の年を取得
 
-        request.setAttribute("classNumList", list);
+		// リクエストパラメーターの取得 2
+		// なし
 
-        request.getRequestDispatcher("student_create.jsp")
-        .forward(request, response); 
-    }
+		// DBからデータ取得 3
+		// ログインユーザーの学校コードをもとにクラス番号の一覧を取得
+		List<String> list = classNumDao.filter(teacher.getSchool());
+
+		// ビジネスロジック 4
+		// リストを初期化
+		List<Integer> entYearSet = new ArrayList<>();
+		// 10年前から10年後まで年をリストに追加
+		for (int i = year - 10; i < year + 11; i++) {
+			entYearSet.add(i);
+		}
+
+		// レスポンス値をセット 6
+		// リクエストにデータをセット
+		req.setAttribute("class_num_set", list);
+		req.setAttribute("ent_year_set", entYearSet);
+
+		// JSPへフォワード 7
+		req.getRequestDispatcher("student_create.jsp").forward(req, res);
+	}
+
 }
