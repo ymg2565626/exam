9de4c0957f506e9a1eaedcb85ae0596de728181d
ymg2565607 <ymg2565607@stu.o-hara.ac.jp>
2026-05-11 10:21:02 +0900

    Update SubjectCreateAction.java

--- src/main/java/scoremanager/main/SubjectCreateAction.java
+++ src/main/java/scoremanager/main/SubjectCreateAction.java
@@ -1,40 +1,17 @@
 package scoremanager.main;
 
-import java.util.List;
-
-import bean.Teacher;
-import dao.ClassNumDAO;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 import tool.Action;
 
-public class SubjectCreateAction extends Action {
+public class StudentCreateAction extends Action {
 
 	@Override
 	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
 
-		HttpSession session = req.getSession(); // セッション
-		Teacher teacher = (Teacher)session.getAttribute("user");
-
-		// ローカル変数の指定 1
-		ClassNumDAO classNumDao = new ClassNumDAO(); // クラス番号Daoを初期化
-
-
-		// リクエストパラメーターの取得 2
-		// なし
-
-		// DBからデータ取得 3
-		// ログインユーザーの学校コードをもとにクラス番号の一覧を取得
-		List<String> list = classNumDao.filter(teacher.getSchool());
-
-		// レスポンス値をセット 6
-		// リクエストにデータをセット
-		req.setAttribute("class_num_set", list);
 
 		// JSPへフォワード 7
-		req.getRequestDispatcher("subject_create.jsp").forward(req, res);
+		req.getRequestDispatcher("student_create.jsp").forward(req, res);
 	}
 
 }
-
