00e8933862f486e2bfa2b21cafce02d547435fcb
suzukikeitarou <ymg2565641@stu.o-hara.ac.jp>
2026-05-14 12:06:44 +0900

    Update SubjectUpdateAction.java

--- src/main/java/scoremanager/main/SubjectUpdateAction.java
+++ src/main/java/scoremanager/main/SubjectUpdateAction.java
@@ -1,5 +1,8 @@
 package scoremanager.main;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import bean.Subject;
 import bean.Teacher;
 import dao.SubjectDAO;
@@ -17,10 +20,17 @@ public class SubjectUpdateAction extends Action {
 		Teacher teacher = (Teacher) session.getAttribute("user");
 		String cd = req.getParameter("cd");
 		SubjectDAO subjectDao = new SubjectDAO();
+		Map<String, String> errors = new HashMap<>();
 
 		Subject subject = subjectDao.get(cd, teacher.getSchool());
+		if (subject == null) {
+			subject = new Subject();
+			subject.setCd(cd);
+			errors.put("1", "科目が存在していません");
+		}
 
 		req.setAttribute("subject", subject);
+		req.setAttribute("errors", errors);
 
 		req.getRequestDispatcher("subject_update.jsp").forward(req, res);
 	}
