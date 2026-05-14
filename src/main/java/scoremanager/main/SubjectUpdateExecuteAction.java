9061a20f431fbe7c3b1ee6a688b396326d9a4cd0
suzukikeitarou <ymg2565641@stu.o-hara.ac.jp>
2026-05-14 11:34:52 +0900

    Update SubjectUpdateExecuteAction.java

--- src/main/java/scoremanager/main/SubjectUpdateExecuteAction.java
+++ src/main/java/scoremanager/main/SubjectUpdateExecuteAction.java
@@ -1,17 +1,31 @@
 package scoremanager.main;
 
+import bean.Subject;
+import bean.Teacher;
+import dao.SubjectDAO;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import tool.Action;
 
 public class SubjectUpdateExecuteAction extends Action {
 
-    public void execute(
-            HttpServletRequest req,
-            HttpServletResponse res
-    ) throws Exception {
+	@Override
+	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
 
-        req.getRequestDispatcher("/SubjectList.action")
-           .forward(req, res);
-    }
+		HttpSession session = req.getSession();
+		Teacher teacher = (Teacher) session.getAttribute("user");
+		String cd = req.getParameter("cd");
+		String name = req.getParameter("name");
+		Subject subject = new Subject();
+		SubjectDAO subjectDao = new SubjectDAO();
+
+		subject.setCd(cd);
+		subject.setName(name);
+		subject.setSchool(teacher.getSchool());
+
+		subjectDao.save(subject);
+
+		req.getRequestDispatcher("subject_update_done.jsp").forward(req, res);
+	}
 }
