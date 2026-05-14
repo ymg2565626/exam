a2458f64e2bd5df52cb68b03dd9abc9b22879ae6
suzukikeitarou <ymg2565641@stu.o-hara.ac.jp>
2026-05-14 11:33:23 +0900

    Update SubjectDeleteExecuteAction.java

--- src/main/java/scoremanager/main/SubjectDeleteExecuteAction.java
+++ src/main/java/scoremanager/main/SubjectDeleteExecuteAction.java
@@ -1,17 +1,29 @@
 package scoremanager.main;
 
+import bean.Subject;
+import bean.Teacher;
+import dao.SubjectDAO;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import tool.Action;
 
 public class SubjectDeleteExecuteAction extends Action {
 
-    public void execute(
-            HttpServletRequest req,
-            HttpServletResponse res
-    ) throws Exception {
+	@Override
+	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
 
-        req.getRequestDispatcher("/SubjectList.action")
-           .forward(req, res);
-    }
+		HttpSession session = req.getSession();
+		Teacher teacher = (Teacher) session.getAttribute("user");
+		String cd = req.getParameter("cd");
+		Subject subject = new Subject();
+		SubjectDAO subjectDao = new SubjectDAO();
+
+		subject.setCd(cd);
+		subject.setSchool(teacher.getSchool());
+
+		subjectDao.delete(subject);
+
+		req.getRequestDispatcher("SubjectList.action").forward(req, res);
+	}
 }
